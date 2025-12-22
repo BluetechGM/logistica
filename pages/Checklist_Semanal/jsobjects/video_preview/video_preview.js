6c783e6102c243bc3c25e5a94d3ff6d28fedb153
suporte bluetech Suporte <suportebluetechfilms@gmail.com>
2025-12-22 19:02:19 +0000

    Imagem de referencia no checklist semanal

--- pages/Checklist_Semanal/jsobjects/video_preview/video_preview.js
+++ pages/Checklist_Semanal/jsobjects/video_preview/video_preview.js
@@ -1,15 +1,15 @@
 export default {
-  getPreviewUrl(url) {
-    if (!url) return "";
+	getPreviewUrl(url) {
+		if (!url) return "";
 
-    // Se for vídeo do Cloudinary
-    if (url.includes("/video/upload/")) {
-      return url
-        .replace("/video/upload/", "/video/upload/so_0/")
-        .replace(/\.(mp4|webm|mov)$/i, ".jpg");
-    }
+		// Se for vídeo do Cloudinary
+		if (url.includes("/video/upload/")) {
+			return url
+				.replace("/video/upload/", "/video/upload/so_0/")
+				.replace(/\.(mp4|webm|mov)$/i, ".jpg");
+		}
 
-    // Caso contrário, assume imagem
-    return url;
-  }
+		// Caso contrário, assume imagem
+		return url;
+	}
 };
